7b56ef7af52439a39d646c12ec75bbee6bfc4c9b
NathShubham28 <nathshubham69@gmail.com>
2024-05-06 17:45:35 +0530

    redirect api change

--- controller/urlController.js
+++ controller/urlController.js
@@ -36,8 +36,8 @@ const redirectUrl = async (req, res) => {
     shortUrl.clicks++
     shortUrl.save()
     console.log(shortUrl)
-    res.status(200).json(shortUrl)
-    // res.redirect(shortUrl.full)
+    // res.status(200).json(shortUrl)
+    res.redirect(shortUrl.full)
 }
 
 module.exports = {createShortUrl, getAllUrl, redirectUrl};
